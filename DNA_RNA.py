9911506ba4ed3da39af400c6dd1be452e8d76c98
Elena Polyakova <enterlina@gmail.com>
2018-11-11 22:17:45 +0300

    Update Function

--- DNA_RNA.py
+++ DNA_RNA.py
@@ -1,21 +1,18 @@
-
-
 class DNA ( ):
 
     def __init__(self):
-
-        print ( "Enter Sequence" )
-        self.sequence = input ( )
-        # self.sequence = 'AGCTTAAAAA'
+        # print ( "Enter Sequence" )
+        # self.sequence = input ( )
+        self.sequence = 'AGCTTAAAAA'
         self.length = len ( self.sequence )
-        self.rna = self.RNA ( )
+        # self.rna = self.RNA ( )
 
     def gc(self):
         self.counter_gc = 0
         for i in range ( 0 , self.length ):
             if (self.sequence[ i ] == 'C') or (self.sequence[ i ] == 'G'):
                 self.counter_gc += 1
-        self.___percent_gc___ = self.counter_gc*100 / self.length
+        self.___percent_gc___ = self.counter_gc * 100 / self.length
         return self.___percent_gc___
 
     def reverse_complement(self):
@@ -26,26 +23,20 @@ class DNA ( ):
 
         return self.___complementary_pair___
 
-    class RNA ( ):
-        def transcribe(self , DNA):
-            self.length = DNA.length
-            self.sequence = DNA.sequence
-            self.transcribe = {'A': 'T' , 'T': 'U' , 'C': 'G' , 'G': 'C'}
-            self.___transcribe_sequence___ = ''
-            for i in range ( 0 , self.length ):
-                self.___transcribe_sequence___ += self.transcribe[ self.sequence[ i ] ]
-            return self.___transcribe_sequence___
+    def transcribe(self):
+        self.transcribe = {'A': 'T' , 'T': 'U' , 'C': 'G' , 'G': 'C', 'U': 'U'}
+        self.___transcribe_sequence___ = ''
+        for i in range ( 0 , self.length ):
+            self.___transcribe_sequence___ += self.transcribe[ self.sequence[ i ] ]
+        return RNA ( self.___transcribe_sequence___)
 
 
 class RNA ( DNA ):
-    pass
-
-    # def transcribe(self):
-    #     self.transcribe = {'A': 'T' , 'T': 'U' , 'C': 'G' , 'G': 'C'}
-    #     self.___transcribe_sequence___ = ''
-    #     for i in range ( 0 , self.length ):
-    #         self.___transcribe_sequence___ += self.transcribe[ self.sequence[ i ] ]
-    #     return self.___transcribe_sequence___
+    def __init__(self, sequence1):
+        # print ( "Enter Sequence" )
+        # self.sequence = input ( )
+        self.sequence = sequence1
+        self.length = len ( self.sequence )
 
 
 print ( '\nDNA' )
@@ -53,11 +44,14 @@ x = DNA ( )
 
 print ( 'GC %' , x.gc ( ) )
 print ( 'Reverse' , x.reverse_complement ( ) )
+print ( 'Transcribe' , x.transcribe ( ))
 
-print ( 'Transcribe' , x.rna.transcribe ( x ) )
 
-print ( '\nRNA' )
-y = RNA ( )
+
+# print ( 'Transcribe' , x.rna.transcribe ( x ) )
+
+# print ( '\nRNA' )
+y = RNA ( "ACUU")
 print ( 'GC %' , y.gc ( ) )
-print ( 'Reverse' , y.reverse_complement ( ) )
-print ( 'Transcribe' , y.rna.transcribe ( x ) )
+# print ( 'Reverse' , y.reverse_complement ( ) )
+print ( 'Transcribe' , y.transcribe ( ) )
