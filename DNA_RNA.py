ec2d824dc01983600c461b33e051f5ba7b778680
Elena Polyakova <enterlina@gmail.com>
2018-11-12 22:09:34 +0300

    update DNA methods

--- DNA_RNA.py
+++ DNA_RNA.py
@@ -7,64 +7,69 @@
 # Создавать объект класса Rna, соответствующий результату транскрипции последовательности оригинального объекта с использованием метода transcribe()
 
 
-class DNA ( ):
+class RNA (str ):
+    def __init__(self , object):
+        self.sequence = object
+        self.length = len ( self.sequence )
+        if set ( self.sequence ) - set ( 'acgutACGUT' ) != set ( ):
+            raise Exception ( '"' + self.sequence + '" ERROR: sequence with wrong letters '
+                              + str ( set ( self.sequence ) - set ( 'acgutACGUT' ) ) )
+
+    def gc(self):
+        counter_gc = 0
+        for i in range ( 0 , self.length ):
+            if (self.sequence[ i ] == 'C') or (self.sequence[ i ] == 'G'):
+                counter_gc += 1
+        percent_gc = counter_gc * 100 / self.length
+        return percent_gc
+
+    def reverse_complement(self):
+        complementary_sequence = {'A': 'U' , 'U': 'A' , 'C': 'G' , 'G': 'C' , 'a': 'u' , 'u': 'a' , 'c': 'g' ,
+                                       'g': 'c'}
+        complementary_pair = list (
+            '' + complementary_sequence[ self.sequence[ i ] ] for i in range ( 0 , self.length ) )
+        return ''.join ( complementary_pair[::-1] )
+
+
+class DNA ( RNA ):
     def __init__(self , object):
         self.sequence = object
         self.length = len ( self.sequence )
         self.rna = self.transcribe ( )
-        if set ( self.sequence ) - set ( 'acgutACGUT' ) != set ( ):
-            raise Exception ( '"' + self.sequence + '" is inappropriate due to the presence of characters '
-                              + str ( set ( self.sequence ) - set ( 'acguACGU' ) ) )
-
-    def gc(self):
-        self.counter_gc = 0
-        for i in range ( 0 , self.length ):
-            if (self.sequence[ i ] == 'C') or (self.sequence[ i ] == 'G'):
-                self.counter_gc += 1
-        self.percent_gc = self.counter_gc * 100 / self.length
-        return self.percent_gc
 
     def reverse_complement(self):
-        self.complementary_sequence = {'A': 'T' , 'T': 'A' , 'C': 'G' , 'G': 'C' , 'a': 't' , 't': 'a' , 'c': 'g' ,
-                                       'g': 'c'}
-        self.complementary_pair = ' '
-        for i in range ( 0 , self.length ):
-            self.complementary_pair += self.complementary_sequence[ self.sequence[ i ] ]
+        complementary_sequence = {'A': 'T' , 'T': 'A' , 'C': 'G' , 'G': 'C' , 'a': 't' , 't': 'a' , 'c': 'g' ,
+                                  'g': 'c'}
+        complementary_pair = list (
+            '' + complementary_sequence[ self.sequence[ i ] ] for i in range ( 0 , self.length ) )
+        return ''.join ( complementary_pair[::-1] )
 
-        return self.complementary_pair
+    def transcribe(self)->RNA:
+        transcribe = {'A': 'U' , 'T': 'A' , 'C': 'G' , 'G': 'C' ,  'a': 'u' , 't': 'a' , 'c': 'g' ,
+                           'g': 'c' }
 
-    def transcribe(self):
-        self.transcribe = {'A': 'T' , 'T': 'U' , 'C': 'G' , 'G': 'C' , 'U': 'U' , 'a': 't' , 't': 'u' , 'c': 'g' ,
-                           'g': 'c' , 'u': 'u'}
-        self.transcribe_sequence = ''
-        for i in range ( 0 , self.length ):
-            self.transcribe_sequence += self.transcribe[ self.sequence[ i ] ]
+        transcribe_sequence = list (''+transcribe[ self.sequence[ i ] ]  for i in range ( 0 , self.length ))
 
-        return RNA ( self.transcribe_sequence )
-
-
-class RNA ( DNA ):
-    def __init__(self , object):
-        self.sequence = object
-        self.length = len ( self.sequence )
+        return RNA ( ''.join (transcribe_sequence[::-1]) )
 
 
 
-sequence1 = 'AGCTTAAAAA'
+#
+# print ( '\nDNA' )
+#
+# sequence1 = 'TAGC'
+# x = DNA ( sequence1 )
+# print ( x.sequence )
+# print ( 'GC %:' , x.gc ( ) )
+# print ( 'Reverse:' , x.reverse_complement ( ) )
+# print ( 'DNA->RNA:' , x.transcribe () )
+#
+#
+# print ( '\nRNA' )
+#
+# sequence2 = 'AUCG'
+# y = RNA ( sequence2 )
+# print ( y.sequence )
+# print ( 'GC %:' , y.gc ( ) )
+# print ( 'Reverse:' , y.reverse_complement ( ) )
 
-print ( '\nDNA' )
-x = DNA ( sequence1 )
-print ( x.sequence )
-print ( 'GC %' , x.gc ( ) )
-print ( 'Reverse' , x.reverse_complement ( ) )
-print ( 'RNA' , x.rna.sequence )
-
-# print ( 'Transcribe' , x.transcribe () )
-# print ( 'Transcribe' , x.rna.transcribe ( x ) )
-
-print ( '\nRNA' )
-
-y = RNA ( sequence1 )
-print ( y.sequence )
-print ( 'GC %' , y.gc ( ) )
-# print ( 'GC %' , y.transcribe( ) )
